ef6fa57859c8e91488dd82dae4e93d7da5f040f9
KI <keita687833@gmail.com>
2023-04-30 09:57:49 +0900

    DAY 6_AjaxとAPIの関係を学ぼう TIPS_02. 「いいね」の状態によって表示を切り替えよう 3-6-5 ハートの表示をコントロールするfunction ハートの表示をコントロールするfuncitonを定義。可読性のため。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,17 +23,22 @@ import $ from 'jquery'
 import axios from 'axios'
 
 
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $('.active-heart').removeClass('hidden')
+  } else {
+    $('.inactive-heart').removeClass('hidden')
+  }
+}
+
+
 document.addEventListener('turbolinks:load', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
+
   axios.get(`/articles/${articleId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
-      if (hasLiked) {
-        $('.active-heart').removeClass('hidden')
-      } else {
-        $('.inactive-heart').removeClass('hidden')
-      }
-      console.log(response)
+      handleHeartDisplay(hasLiked)
     })
 })
